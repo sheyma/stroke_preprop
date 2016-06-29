786aa433364f2d8a10ca319550993f73abe246c3
Şeyma Bayrak <sheymaba@gmail.com>
2016-06-29 18:25:21 +0200

    test run for one subject, and its multiple subdirs

--- convert.py
+++ convert.py
@@ -8,34 +8,39 @@ import nipype.interfaces.io as nio
 from nipype.interfaces.io import DataSink, SelectFiles
 
 data_dir = '/scr/ilz2/bayrak/32_COIL_ordered/'
-working_dir = '/scr/ilz2/bayrak/test/'
+working_dir = '/scr/ilz2/bayrak/32_COIL_nifti/'
+
+subject_id = sys.argv[1]
+
+scan_path = os.path.join(data_dir, subject_id)
+print scan_path
+scans = next(os.walk(scan_path))[1]
+print ''
+print scans
 
 
+for scan in scans:
+    
+  wflow = Workflow(name='wflow')
+  wflow.base_dir = working_dir
+  wflow.config['execution']['crashdump_dir'] = wflow.base_dir + "/crash_files"
 
+  file_template = {'test' : subject_id + '/' + scan +'/*dcm'}
 
-wflow = Workflow(name='wflow')
-wflow.base_dir = working_dir
-wflow.config['execution']['crashdump_dir'] = wflow.base_dir + "/crash_files"
+  selectfiles = Node(SelectFiles(file_template, base_directory=data_dir),
+		    name="selectfiles")
 
-file_template = {'test' : '01/DWId00/*dcm'}
+  stacker = Node(DcmStack(embed_meta=True),
+		name='stacker')
+  stacker.inputs.out_format = scan
 
+  ds_dir = working_dir
+  ds = Node(DataSink(), name='ds')
+  ds.inputs.base_directory = ds_dir
 
-selectfiles = Node(SelectFiles(file_template, base_directory=data_dir),
-                   name="selectfiles")
+  wflow.connect(selectfiles, 'test', stacker, 'dicom_files')
+  wflow.connect(stacker, 'out_file' , ds, subject_id)
 
-
-stacker = Node(DcmStack(embed_meta=True),
-	       name='stacker')
-
-stacker.inputs.out_format = 'DWId00'
-
-ds_dir = os.path.join(working_dir, 'OUT')
-ds = Node(DataSink(), name='ds')
-ds.inputs.base_directory = ds_dir
-
-wflow.connect(selectfiles, 'test', stacker, 'dicom_files')
-wflow.connect(stacker, 'out_file' , ds, 'bla')
-
-wflow.run()
+  wflow.run()
 
 
