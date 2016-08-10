4b33fa7002f662f4611b79a4878ec201a0611e4e
Şeyma Bayrak <sheymaba@gmail.com>
2016-08-10 20:03:01 +0200

    rest2mni registration for stroke data

--- 06_funcRegis.py
+++ 06_funcRegis.py
@@ -2,72 +2,42 @@ import os, sys
 import nipype.interfaces.ants as ants
 
 data_in = '/scr/ilz2/bayrak/stroke_bbreg'
-data_out = '/scr/ilz2/bayrak/stroke_rs2mni'
+data_out = '/scr/ilz2/bayrak/stroke_rest2mni'
+data_ants = '/scr/ilz2/bayrak/stroke_ants'
+mni_dir = '/usr/share/fsl/5.0/data/standard'
 
-subject_id = '01'
-scan = 'rsd01'
-Tscan = 'T1d01'
+subject_id = sys.argv[1]
+scan = sys.argv[2]
+Tscan = sys.argv[3]
 
 if not os.path.exists(os.path.join(data_out, subject_id)):
 	os.makedirs(os.path.join(data_out, subject_id))
 work_dir = os.path.join(data_out, subject_id)
-if not os.path.exists(os.path.join(work_dir, scan)):
-	os.makedirs(os.path.join(work_dir, scan))
-work_dir = os.path.join(work_dir, scan)
+if not os.path.exists(os.path.join(work_dir, scan + '_' + Tscan)):
+	os.makedirs(os.path.join(work_dir, scan + '_' + Tscan))
+work_dir = os.path.join(work_dir, scan + '_' + Tscan)
 
 # go into working directory
 os.chdir(work_dir)
 
 # find functional data to be registered
-img_rest = os.path.join(data_in, subject_id, scan, 'rest_ss.nii.gz')
+img_rest = os.path.join(data_in, subject_id, scan + '_' + Tscan, 
+			'rest_ss.nii.gz')
+
 os.system("cp %s %s" % (img_rest, work_dir))
 
-# MNI template (no skull)
-mni_temp = os.path.join('/usr/share/fsl/5.0/data/standard', 
-			'MNI152_T1_1mm_brain.nii.gz')
-
-# specify ants registration out dir
-ants_reg = os.path.join('/scr/ilz2/bayrak/stroke_ants/',
-			subject_id, Tscan)
-
-# apply all transforms
-print "WORK DIR", work_dir
-
-#at = ants.ApplyTransforms()
-##at.inputs.dimension = 3
-#at.inputs.input_image_type = 3
-#at.inputs.input_image = '/scr/ilz2/bayrak/stroke_rs2mni/01/rsd01/rest_ss.nii.gz'
-#at.inputs.transforms = '/scr/ilz2/bayrak/stroke_bbreg/01/rsd01/rest2anat_itk.mat'
-#at.inputs.reference_image = '/scr/ilz2/bayrak/stroke_bbreg/01/rsd01/brain.nii.gz'
-#at.inputs.interpolation = 'BSpline'
-#at.inputs.invert_transform_flags = [False]
-#at.inputs.output_image = '/scr/ilz2/bayrak/stroke_rs2mni/01/rsd01/rest_01.nii.gz'
-#print at.cmdline
-#at.run()
-
-
+# apply all transform matrices ($bbregister & $ants.Register) on rs-image
 at = ants.ApplyTransforms()
-#at.inputs.dimension = 3
 at.inputs.input_image_type = 3
-at.inputs.input_image = '/scr/ilz2/bayrak/stroke_rs2mni/01/rsd01/rest_01.nii.gz'
-at.inputs.transforms = ['/scr/ilz2/bayrak/stroke_ants/01/T1d01/transform1InverseWarp.nii.gz',
-			'/scr/ilz2/bayrak/stroke_ants/01/T1d01/transform0GenericAffine.mat']
-at.inputs.reference_image = mni_temp
+at.inputs.input_image = 'rest_ss.nii.gz'
+at.inputs.transforms = [os.path.join(data_ants, subject_id, Tscan, 'transform1Warp.nii.gz'),
+			os.path.join(data_ants, subject_id, Tscan, 'transform0GenericAffine.mat'),
+			os.path.join(data_in, subject_id, scan+'_'+Tscan, 'rest2anat_itk.mat')]
+at.inputs.reference_image = os.path.join(mni_dir, 'MNI152_T1_2mm_brain.nii.gz')
 at.inputs.interpolation = 'BSpline'
-at.inputs.invert_transform_flags = [False, False]
-at.inputs.output_image = '/scr/ilz2/bayrak/stroke_rs2mni/01/rsd01/rest_02.nii.gz'
+at.inputs.invert_transform_flags = [False, False, False]
+at.inputs.output_image = 'rest_mni.nii.gz'
 print at.cmdline
 at.run()
 
 
-#applytransform = Node(ants.ApplyTransforms(input_image_type = 3,
-#                                           #output_image='rest_preprocessed2mni.nii.gz',
-#                                           interpolation = 'BSpline',
-#                                           invert_transform_flags=[False, False]),
-#                      name='applytransform')
-   
-#applytransform.inputs.reference_image=template
-#applytransform.plugin_args={'submit_specs': 'request_memory = 30000'}
-#mni.connect([(selectfiles, applytransform, [('rest', 'input_image')]),
- #            (translist, applytransform, [('out', 'transforms')])
-#])
