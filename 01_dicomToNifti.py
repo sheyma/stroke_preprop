9dd832503f09e567eafa540709be77960900b21d
Şeyma Bayrak <sheymaba@gmail.com>
2016-08-12 16:53:53 +0200

    nifti convertion with inhouse tool isisconv

--- 01_dicomToNifti.py
+++ 01_dicomToNifti.py
@@ -2,7 +2,7 @@ import os, sys
 from subprocess import call
 
 data_dir = '/scr/ilz2/bayrak/32_COIL_ordered/'
-data_out = '/scr/ilz2/bayrak/stroke_nifti/'
+data_out = '/scr/ilz2/bayrak/tmp/'
 
 subject_id = sys.argv[1]
 
@@ -16,10 +16,13 @@ print ''
 for scan in scans:
 
 	dicom_files = os.path.join(data_dir, subject_id, scan)
+	dicom_files = dicom_files + '/*dcm'
 
-	if not os.path.exists(os.path.join(data_out, subject_id)):
-		os.makedirs(os.path.join(data_out, subject_id))
-	work_dir = os.path.join(data_out, subject_id)
+	sbj = 'sd' + subject_id
+
+	if not os.path.exists(os.path.join(data_out, sbj)):
+		os.makedirs(os.path.join(data_out, sbj))
+	work_dir = os.path.join(data_out, sbj)
 
 	if not os.path.exists(os.path.join(work_dir, scan)):
 		os.makedirs(os.path.join(work_dir, scan))
@@ -28,8 +31,15 @@ for scan in scans:
 	os.chdir(work_dir)
 
 	output_name = 'unknown'
+
 	if scan[0:3] == 'rsd':
-		output_name = 'rest'
+
+		output_name = 'rest.nii'
+		print dicom_files
+
+		os.system("isisconv -in %s -wdialect fsl -out %s" % (dicom_files, output_name))
+		os.system("gzip %s" % (output_name))
+
 	if scan[0:5] == 'FLAIR':
 		output_name = 'flair'
 	if scan[0:2] == 'DW':
@@ -37,15 +47,18 @@ for scan in scans:
 	if scan[0:2] == 'T1':
 		output_name = 'T1'
 
-	print "converting files ", dicom_files, "..."
-	call(["dcmstack", dicom_files,
-		"--dest-dir", work_dir,
-		"-o", output_name])
+	#print "converting files ", dicom_files, "..."
 	
-	img_nifti = os.path.join(output_name + '.nii.gz')
+	#A =["isisconv", "-in", dicom_files, "-wdialect", "fsl", "-out", work_dir+'/'+output_name+'.nii']
+	#print A
+
+	#call(["isisconv", "-in", dicom_files, "-wdialect", "fsl"
+	#      "-out", work_dir+'/'+output_name+'.nii'])
+	
+	#img_nifti = os.path.join(output_name + '.nii.gz')
 
 	# change the orientation of nifti-1 file
-	img_RPI = output_name + '.nii.gz'
-	os.system("fslswapdim %s RL PA IS %s" % (img_nifti, img_RPI))
+	#img_RPI = output_name + '.nii.gz'
+	#os.system("fslswapdim %s RL PA IS %s" % (img_nifti, img_RPI))
 
 
