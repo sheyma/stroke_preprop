6f5536fc8445718bf59b6b45a1db3aa8168ea3ca
Şeyma Bayrak <sheymaba@gmail.com>
2016-08-23 21:29:37 +0200

    data_dir

--- structReconAll.sh
+++ structReconAll.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 path_run=$HOME/devel/stroke_preprop
-subj_dir='/scr/ilz2/bayrak/stroke_nifti'
-out_dir='/scr/ilz2/bayrak/stroke_reconall'
+subj_dir='/scr/ilz2/bayrak/new_nifti'
+out_dir='/scr/ilz2/bayrak/new_struc'
 outfile="$path_run/structReconAll"
 
 echo "executable = $path_run/setenv.sh" > $outfile
@@ -12,37 +12,24 @@ echo "getenv = True" >> $outfile
 echo "notification = Error" >> $outfile
 
 
-#for i in $subj_dir/*; do
-#
-#	subj=$(basename $i) 	
-#
-#	for j in $subj_dir/$subj/T*; do
-#		
-#		scan=$(basename $j) 
-#		out="$out_dir/${subj}_${scan}"
-#
-#		echo "arguments = /usr/bin/time -v python $path_run/02_structReconAll.py $subj $scan";
-#		echo "output = $out.out"
-#		echo "error = $out.out" # stderr and stdout into the same file
-#		echo "log = $out.log"
-#		echo "queue" 
-#		echo ""
-#	done ;
-#
-#done >> $outfile 
+for i in $subj_dir/*; do
 
+	subj=$(basename $i) 	
 
-for subj in 41 43 44 45 46 48 49 ; do		
+	for j in $subj_dir/$subj/T*; do
+		
+		scan=$(basename $j) 
+		out="$out_dir/${subj}_${scan}"
 
-	scan='T1d00' 
-	out="$out_dir/${subj}_${scan}"
-
-	echo "arguments = /usr/bin/time -v python $path_run/02_structReconAll.py $subj $scan";
-	echo "output = $out.out"
-	echo "error = $out.out" # stderr and stdout into the same file
-	echo "log = $out.log"
-	echo "queue" 
-	echo ""
+		echo "arguments = /usr/bin/time -v python $path_run/02_structReconAll.py $subj $scan";
+		echo "output = $out.out"
+		echo "error = $out.out" # stderr and stdout into the same file
+		echo "log = $out.log"
+		echo "queue" 
+		echo ""
+	done ;
 
 done >> $outfile 
+
+
 
