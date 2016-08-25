73c94f18d51d8bcb0ed9eb99777e7c24aa19b8ea
Şeyma Bayrak <sheymaba@gmail.com>
2016-08-25 12:15:33 +0200

    get slice acquisition times from dicom image

--- scan_info.py
+++ scan_info.py
@@ -16,6 +16,9 @@ filenames = arguments.files
 #filenames = ['/scr/ilz2/bayrak/32_COIL_ordered/01/T1d01/t1-2_97.dcm',
 #             '/scr/ilz2/bayrak/32_COIL_ordered/01/T1d01/t1-2_97.dcm']
 
+#filenames = ['/scr/ilz2/bayrak/TEST/rs1_9.dcm']
+
+
 def getParams(filename):
     f = dicom.read_file(filename)
     return f.EchoNumbers ,\
@@ -68,7 +71,31 @@ df = pd.DataFrame({'KEYS': ['EchoNumbers',
                               'Rows',
                               'Depth'],
                   'VALUES' : new_params})   
-                              
-df.to_csv(OutFile, index=False, cols=('KEYS', 'VALUES'), sep='\t')    
+A = filenames[0]
+print A
 
-print "OUTFILE", OutFile
+from dcmstack.extract import default_extractor
+from dicom import read_file
+from nipype.utils.filemanip import filename_to_list
+
+def get_info(dicom_files):
+	"""Given a Siemens dicom file return metadata
+	Returns
+	-------
+	RepetitionTime
+	Slice Acquisition Times
+	Spacing between slices
+	"""
+	meta = default_extractor(read_file(filename_to_list(dicom_files)[0],
+		                       stop_before_pixels=True,
+		                       force=True))
+	#print meta
+
+	return (meta['RepetitionTime'] / 1000., meta['CsaImage.MosaicRefAcqTimes'],
+		meta['SpacingBetweenSlices'])
+
+print get_info(A)   
+                           
+#df.to_csv(OutFile, index=False, cols=('KEYS', 'VALUES'), sep='\t')    
+
+#print "OUTFILE", OutFile
