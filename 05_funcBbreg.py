1344ed9671032baca0ac09e509cd958f095a925b
Şeyma Bayrak <sheymaba@gmail.com>
2016-09-05 12:24:13 +0200

    change data_dir's and implement mri_convert by calling through nipype

--- 05_funcBbreg.py
+++ 05_funcBbreg.py
@@ -5,8 +5,7 @@ from subprocess import call
 from nipype.interfaces.c3 import C3dAffineTool
 
 # data dir's 
-data_in  = '/scr/ilz2/bayrak/new_func/'
-data_out = '/scr/ilz2/bayrak/new_bbreg/'
+data_dir  = '/scr/ilz2/bayrak/preprocess/'
 
 # subject id, resting scan, T1 scan
 subject_id = sys.argv[1]
@@ -14,29 +13,25 @@ scan 	   = sys.argv[2]
 Tscan 	   = sys.argv[3]
 
 # freesurfer dir for recon_all outputs 
-free_dir = '/scr/ilz2/bayrak/new_struc'
+freesurfer_dir = os.path.join(data_dir, subject_id, Tscan)
 
-freesurfer_dir = os.path.join(free_dir, subject_id)
-freesurfer_dir = os.path.join(freesurfer_dir, Tscan)	
+# define working dir
+rs_T1    = scan+'_'+Tscan
+work_dir = os.path.join(data_dir, subject_id, rs_T1, 
+			'bbregister') 
 
-# create a working directory	
-if not os.path.exists(os.path.join(data_out, subject_id)):
-	os.makedirs(os.path.join(data_out, subject_id))
-work_dir = os.path.join(data_out, subject_id)
-# dir for 'resting scan registered to T1 scan'
-rs_T1 = scan+'_'+Tscan
-if not os.path.exists(os.path.join(work_dir, rs_T1)):
-	os.makedirs(os.path.join(work_dir, rs_T1))
-work_dir = os.path.join(work_dir, rs_T1)
+if not os.path.exists(work_dir):
+	os.makedirs(work_dir)
 
-# go into working directory
+# go into working dir
 os.chdir(work_dir)
 
 # get motion and slice-time corrected rs image
-data_in_absol = os.path.join(data_in, subject_id, scan)
-img_rest = os.path.join(data_in_absol, 'corr_rest_roi.nii.gz')
+data_prepro = os.path.join(data_dir, subject_id, 
+			     scan, 'func_prepro')
+
+img_rest = os.path.join(data_prepro, 'corr_rest_roi.nii.gz')
 print img_rest
-#os.system("cp %s %s" % (img_nifti, work_dir))
 
 # get T-mean of rs image
 fslmaths = MeanImage()
@@ -59,11 +54,16 @@ bbreg.inputs.epi_mask 	     = True
 bbreg.run()
 
 # get structural image
-img_struc = os.path.join(free_dir, subject_id, Tscan, 
+img_struc = os.path.join(freesurfer_dir, 
 			 'recon_all/mri/brain.mgz') 
 
 # convert structural image into nifti 
-os.system("mri_convert %s %s" % (img_struc, 'brain.nii.gz'))
+from nipype.interfaces.freesurfer.preprocess import  MRIConvert
+mc = MRIConvert()
+mc.inputs.in_file = img_struc
+mc.inputs.out_file = 'brain.nii.gz'
+mc.inputs.out_type = 'niigz'
+mc.run()
 		
 # convert bbregister out into itk format for ants later...
 c3 = C3dAffineTool()
