5a35d6e025bb2130bdbff6fdc05512c827b5b55b
Şeyma Bayrak <sheymaba@gmail.com>
2016-10-14 14:54:09 +0200

    get a mask for brain.nii.gz

--- 04_structAnts.py
+++ 04_structAnts.py
@@ -1,44 +1,66 @@
 """
 non-linear registration from T1 to MNI
 How to run:
-$ python 04_structAnts.py hc01 T1d00
+$ python 04_structAnts.py hc01_d00
 """
 import os, sys
 import nipype.interfaces.ants as ants
+import nipype.interfaces.freesurfer as fs
+import nipype.interfaces.fsl as fsl
 from subprocess import call
 
 # data dir's 
-data_dir   = '/scr/ilz2/bayrak/preprocess/'
-temp_dir   = 'ants_all'
-recon_path = 'recon_all/mri'
+data_dir   = '/nobackup/ilz2/bayrak/subjects'
 
-# user given subject_id and T1 scan
+# user given subject_id 
 subject_id = sys.argv[1]
-Tscan 	   = sys.argv[2]
 
 # MNI template (no skull)
 mni_temp = os.path.join('/usr/share/fsl/5.0/data/standard', 
 			'MNI152_T1_1mm_brain.nii.gz')
 
 # define working dir
-work_dir = os.path.join(data_dir, subject_id, 
-			Tscan, temp_dir)
+work_dir = os.path.join(data_dir, subject_id,  
+			'preprocessed/anat')
 if not os.path.exists(work_dir):
 	os.makedirs(work_dir)
 	
 # go into work dir
 os.chdir(work_dir)
 
-# get structural image
-img_struc = os.path.join(data_dir, subject_id, Tscan, 
-			 recon_path, 'brain.mgz') 
+# get skull stripped (recon_all) structural image
+img_struc = os.path.join(data_dir, subject_id,  
+			 'freesurfer/mri', 'brain.mgz') 
 
 # convert structural image into nifti 
 os.system("mri_convert %s %s" % (img_struc, 'brain.nii.gz'))
 img_nifti = os.path.abspath('brain.nii.gz')
 
+# create  brain mask 
+get_mask = fs.Binarize()
+get_mask.inputs.in_file     = 'brain.nii.gz'
+get_mask.inputs.min         = 0.5
+get_mask.inputs.dilate      = 1
+get_mask.inputs.out_type    = 'nii.gz'
+get_mask.inputs.binary_file = 'brain_mask.nii.gz'
+get_mask.run()
+
+## fill holes in mask, smooth, rebinarize
+#fillholes = fsl.maths.MathsCommand()
+
+#fillholes = fsl.maths.MathsCommand(args='-fillh -s 3 -thr 0.1 -bin')
+#fillholes.inputs.in_file = 'brain_mask.nii.gz'
+#fillholes.out_file = 'bla.nii.gz'
+#fillholes.run()
+
 # define ants output 
 img_ants = os.path.abspath('brain_mni.nii.gz')
+
+# define working dir for transform matrices
+work_dir_trf = os.path.join(data_dir, subject_id,  
+			    'preprocessed/anat/transforms2mni')
+if not os.path.exists(work_dir_trf):
+	os.makedirs(work_dir_trf)
 
 ants_anat2mni = ants.Registration(dimension=3,
 	            transforms=['Rigid','Affine','SyN'],
