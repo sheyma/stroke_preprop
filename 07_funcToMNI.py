a2f4681d31ffabb6069e676a8b4238d11054b980
Şeyma Bayrak <sheymaba@gmail.com>
2016-10-20 20:05:04 +0200

    minor indentation changes

--- 07_funcToMNI.py
+++ 07_funcToMNI.py
@@ -1,55 +1,41 @@
 import os, sys
 import nipype.interfaces.ants as ants
 
-# data dir's
-data_dir = '/scr/ilz2/bayrak/preprocess/'
-mni_dir  = '/scr/ilz2/bayrak'
-
-# subject id, resting scan, T1 scan
+# data dir's 
+data_dir = '/nobackup/ilz2/bayrak/subjects'
+mni_dir  = '/nobackup/ilz2/bayrak'
+# subject id
 subject_id = sys.argv[1]
-scan 	   = sys.argv[2]
-Tscan      = sys.argv[3]
-
-# create a working directory	
-rs_T1 = scan+'_'+Tscan
 
 # define working dir
-rs_T1    = scan+'_'+Tscan
-
-work_dir = os.path.join(data_dir, subject_id, rs_T1, 
-			'func2mni') 
+work_dir = os.path.join(data_dir, subject_id, 'preprocessed/func') 
 if not os.path.exists(work_dir):
 	os.makedirs(work_dir)
-
-# go into working directory
+# go into working dir
 os.chdir(work_dir)
 
 # data dir's to
-ants_dir = os.path.join(data_dir, subject_id, Tscan, 
-			'ants_all')
-func_dir = os.path.join(data_dir, subject_id, rs_T1,
-			'denoise')
-bbre_dir = os.path.join(data_dir, subject_id, rs_T1,
-			'bbregister')
+ants_dir = os.path.join(data_dir, subject_id,  
+			'preprocessed/anat/transforms2mni')
+bbre_dir = os.path.join(data_dir, subject_id,
+			'preprocessed/func/coregister/transforms2anat')
 
-# find functional data to be registered
+# get preprocessed functional data to be registered
+img_rest = os.path.join(data_dir, subject_id,
+			'preprocessed/func/', 'rest_preprocessed.nii.gz')
 
-img_rest = os.path.join(func_dir,
-			'corr_rest_roi_denoised.nii.gz')
-
-# apply all transform matrices ($bbregister & $ants.Register) 
-# on resting-state image
+# coregistering img_rest to the mni 3mm space
 at			   = ants.ApplyTransforms()
 at.inputs.input_image_type = 3
 at.inputs.input_image      = img_rest
-at.inputs.transforms	   = [os.path.join(ants_dir, 						   'transform1Warp.nii.gz'),
-              		      os.path.join(ants_dir, 						   'transform0GenericAffine.mat'),
-         		      os.path.join(bbre_dir, 						   'rest2anat_itk.mat')]
+at.inputs.transforms	   = [os.path.join(ants_dir, 						   						   'transform1Warp.nii.gz'),
+              		      os.path.join(ants_dir, 						   						   'transform0GenericAffine.mat'),
+         		      os.path.join(bbre_dir, 						   						   'rest2anat_itk.mat')]
 at.inputs.reference_image  = os.path.join(mni_dir, 
 					  'MNI152_T1_3mm_brain.nii.gz')
 at.inputs.interpolation    = 'BSpline'
 at.inputs.invert_transform_flags = [False, False, False]
-at.inputs.output_image     = os.path.abspath('rest_mni.nii.gz')
+at.inputs.output_image     = os.path.abspath('rest_preprocessed2mni.nii.gz')
 #print at.cmdline
 at.run()
 
