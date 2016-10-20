8aa3ebc90f6326f888b414b214dd80b190fe2e4b
Şeyma Bayrak <sheymaba@gmail.com>
2016-10-20 19:25:28 +0200

    change subject_id from 'recon_all' to the real one

--- 05_funcBbreg.py
+++ 05_funcBbreg.py
@@ -1,75 +1,65 @@
 import os, sys
-from nipype.interfaces.fsl.maths import MeanImage
 from nipype.interfaces.freesurfer import BBRegister
-from subprocess import call
 from nipype.interfaces.c3 import C3dAffineTool
 
 # data dir's 
-data_dir  = '/scr/ilz2/bayrak/preprocess/'
+data_dir  = '/nobackup/ilz2/bayrak/subjects'
 
-# subject id, resting scan, T1 scan
+# subject id
 subject_id = sys.argv[1]
-scan 	   = sys.argv[2]
-Tscan 	   = sys.argv[3]
 
-# freesurfer dir for recon_all outputs 
-freesurfer_dir = os.path.join(data_dir, subject_id, Tscan)
+# freesurfer dir for recon_all outputs
+freesurfer_dir = '/nobackup/ilz2/bayrak/freesurfer'
 
 # define working dir
-rs_T1    = scan+'_'+Tscan
-work_dir = os.path.join(data_dir, subject_id, rs_T1, 
-			'bbregister') 
-
+work_dir = os.path.join(data_dir, subject_id,  
+			'preprocessed/func/coregister') 
 if not os.path.exists(work_dir):
 	os.makedirs(work_dir)
-
 # go into working dir
 os.chdir(work_dir)
 
-# get motion and slice-time corrected rs image
-data_prepro = os.path.join(data_dir, subject_id, 
-			     scan, 'func_prepro')
+# Step#1 coregistering rest scan to T1
+# get mean rest scan after alignment
+rest_mean  = os.path.join(data_dir, subject_id,
+			  'preprocessed/func/realign',
+			  'mean_corr_rest_roi.nii.gz')
 
-img_rest = os.path.join(data_prepro, 'corr_rest_roi.nii.gz')
-print img_rest
+# define names for the bbregister outputs
+reg_file  = os.path.abspath('rest2anat_highRes.nii.gz')
+cost_file = os.path.abspath('rest2anat.dat.mincost')
 
-# get T-mean of rs image
-fslmaths = MeanImage()
-fslmaths.inputs.in_file   = img_rest
-fslmaths.inputs.out_file  = 'rest_mean.nii.gz'
-fslmaths.inputs.dimension = 'T'
-fslmaths.run()
+# define working dir for transform matrices
+work_dir_trf = os.path.abspath('transforms2anat') 
+if not os.path.exists(work_dir_trf):
+	os.makedirs(work_dir_trf)
+
+# go into working dir
+os.chdir(work_dir_trf)
 
 # do bbregister
 bbreg = BBRegister()
 bbreg.inputs.contrast_type   = 't2'
-bbreg.inputs.source_file     = 'rest_mean.nii.gz'
+bbreg.inputs.source_file     = rest_mean
 bbreg.inputs.init 	     = 'fsl'
 bbreg.inputs.subjects_dir    = freesurfer_dir
-bbreg.inputs.subject_id      = 'recon_all'
+bbreg.inputs.subject_id      = subject_id
 bbreg.inputs.out_reg_file    = 'rest2anat.dat'
 bbreg.inputs.out_fsl_file    = 'rest2anat.mat'
-bbreg.inputs.registered_file = 'rest2anat_highRes.nii.gz'
+bbreg.inputs.registered_file = reg_file
 bbreg.inputs.epi_mask 	     = True
 bbreg.run()
 
-# get structural image
-img_struc = os.path.join(freesurfer_dir, 
-			 'recon_all/mri/brain.mgz') 
+# Step#2 formatting bbregister output
+# get structural reference image
+ref_struc = os.path.join(data_dir, subject_id,
+			 'preprocessed/anat', 'brain.nii.gz')
 
-# convert structural image into nifti 
-from nipype.interfaces.freesurfer.preprocess import  MRIConvert
-mc = MRIConvert()
-mc.inputs.in_file = img_struc
-mc.inputs.out_file = 'brain.nii.gz'
-mc.inputs.out_type = 'niigz'
-mc.run()
-		
-# convert bbregister out into itk format for ants later...
+# convert bbregister out into itk format for ants later
 c3 = C3dAffineTool()
 c3.inputs.transform_file  = 'rest2anat.mat'
 c3.inputs.itk_transform   = 'rest2anat_itk.mat'
-c3.inputs.reference_file  = 'brain.nii.gz'
-c3.inputs.source_file     = 'rest_mean.nii.gz'
+c3.inputs.reference_file  = ref_struc
+c3.inputs.source_file     = rest_mean
 c3.inputs.fsl2ras         = True
 c3.run()
