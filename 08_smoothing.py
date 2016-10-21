20c630e7f0ba0233e5cf98f3da3fcfbf428d9ae6
Şeyma Bayrak <sheymaba@gmail.com>
2016-10-21 15:22:55 +0200

    change working_dir's and filenames

--- 08_smoothing.py
+++ 08_smoothing.py
@@ -1,50 +1,49 @@
 import os, sys
 import nipype.interfaces.fsl as fsl
 
-# data dir's
-data_dir  = '/nobackup/ilz2/bayrak/preprocess'
-data_out  = 'smooth'
+# data dir's 
+data_dir  = '/nobackup/ilz2/bayrak/subjects'
 
-# subject id, resting scan, T1 scan
+# subject id
 subject_id = sys.argv[1]
-scan 	   = sys.argv[2]
-Tscan      = sys.argv[3]
 
 # define working dir
-rs_T1    = scan+'_'+Tscan
-
-work_dir = os.path.join(data_dir, subject_id, rs_T1, 
-			data_out) 
+work_dir = os.path.join(data_dir, subject_id, 
+			'preprocessed/func') 
 if not os.path.exists(work_dir):
 	os.makedirs(work_dir)
-
-# go into working directory
+# go into working dir
 os.chdir(work_dir)
 
+rest_mni = os.path.join(work_dir,
+			'rest_preprocessed2mni.nii.gz')
 
-rest_fun2mni = os.path.join(data_dir, subject_id, rs_T1,
-			    'func2mni', 'rest_mni.nii.gz')
-print rest_fun2mni
+# define working dir for smoothing
+work_dir_smooth = os.path.join(os.getcwd(), 'smooth')
+if not os.path.exists(work_dir_smooth):
+	os.makedirs(work_dir_smooth)
+os.chdir(work_dir_smooth)
+
 # Step#1 get a brain mask for func2mni image 
 from nipype.interfaces import afni as afni
 automask = afni.Automask()
-automask.inputs.in_file    = rest_fun2mni 
+automask.inputs.in_file    = rest_mni 
 automask.inputs.outputtype = "NIFTI_GZ"
 automask.run()
 
 # Step#2 smooth func2mni image
 from nipype.interfaces.fsl import maths
 smooth = maths.IsotropicSmooth()
-smooth.inputs.in_file = rest_fun2mni
+smooth.inputs.in_file = rest_mni
 smooth.inputs.fwhm    = 6
 smooth.run()
-print smooth.cmdline
 
-# Step#3 mask the smoothed image
+## Step#3 mask the smoothed image
 from nipype.interfaces.fsl import maths
 maskApp = maths.ApplyMask()
-maskApp.inputs.in_file     = 'rest_mni_smooth.nii.gz'
-maskApp.inputs.mask_file   = 'rest_mni_mask.nii.gz'
-maskApp.inputs.out_file    = 'rest_mni_smooth_masked.nii.gz'
+maskApp.inputs.in_file     = 'rest_preprocessed2mni_smooth.nii.gz'
+maskApp.inputs.mask_file   = 'rest_preprocessed2mni_mask.nii.gz'
+maskApp.inputs.out_file    = os.path.join(work_dir, 
+		             'rest_preprocessed2mni_sm.nii.gz')
 maskApp.inputs.output_type = 'NIFTI_GZ'
 maskApp.run()
