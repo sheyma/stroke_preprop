cfaa5527b740d0a69a1d9da59ab2bf69641e6776
Şeyma Bayrak <sheymaba@gmail.com>
2016-11-07 14:54:06 +0100

    create GM mask at group level

---
+++ 10_greyMatterGroup.py
@@ -0,0 +1,53 @@
+"""
+get GM mask at group level 
+"""
+import os, sys
+from nipype.interfaces.fsl import MultiImageMaths
+from nipype.interfaces.fsl.maths import MathsCommand
+
+# data dir's 
+data_dir  = '/nobackup/ilz2/bayrak/subjects'
+
+# write subject-id's into a list
+fname = '/nobackup/ilz2/bayrak/documents/cool_hc.txt'
+with open(fname) as f:
+    content = f.readlines()
+sbj_list = [x.strip('\n') for x in content]
+
+# write GM mask filenames into a list
+msk_list = []
+tmp_str  = []
+i = 0
+for sbj in sbj_list:
+	msk_sbj = os.path.join(data_dir, sbj,
+			       'preprocessed/func/connectivity',
+			       'gm_mask_mni3.nii.gz')
+	msk_list.append(msk_sbj)
+	i += 1
+	if i != 1:
+		tmp_str.append('-add %s ') 
+
+tmp_str.append(('-div %s' % len(msk_list)))	
+op_string = " ".join((tmp_str))
+
+# get average probabilistic GM of all subjects
+gm_ave_grp = os.path.join('/nobackup/ilz2/bayrak/subjects_group',
+			   'gm_mni3_ave.nii.gz')
+maths = MultiImageMaths()
+maths.inputs.in_file       = msk_list[0]
+maths.inputs.op_string     = op_string
+maths.inputs.operand_files = msk_list[1:]
+maths.inputs.out_file      = gm_ave_grp
+maths.run()
+
+# get mask out of average probabilistic GM via thresholding
+gm_ave_mask = os.path.join('/nobackup/ilz2/bayrak/subjects_group',
+			   'gm_mni3_ave_mask.nii.gz')
+
+binarize = MathsCommand()
+binarize.inputs.args     = '-thr  0.5 -bin'
+binarize.inputs.in_file  = gm_ave_grp
+binarize.inputs.out_file = gm_ave_mask
+binarize.run()
+
+
