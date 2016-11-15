6160c0d50578865b45f9b859d638772ab55d3181
Şeyma Bayrak <sheymaba@gmail.com>
2016-11-15 12:43:28 +0100

    add function to check zeros-voxels in all subjects

--- 11_corrUpper.py
+++ 11_corrUpper.py
@@ -21,15 +21,36 @@ def mask_check(rest, mask):
 			cnt_zeros += 1
 	return cnt_zeros, matrix
 
+def mask_check_all_subjects(fname, data_dir, mask):
+	"""
+	fname: text file having subject_id's
+	"""
+	with open(fname) as f:
+		content = f.readlines()
+		sbj_list = [x.strip('\n') for x in content]
+
+	for subject_id in sbj_list:
+		rest = os.path.join(data_dir, subject_id,
+	                            'preprocessed/func', 
+	                            'rest_preprocessed2mni_sm.nii.gz')
+		[cnt_zeros, matrix] = mask_check(rest, mask)
+		print subject_id, cnt_zeros
+
 # data dir's 
 data_dir  = '/nobackup/ilz2/bayrak/subjects'
 
-# subject_id
-subject_id = sys.argv[1]
+# text file having subject_id's
+fname = '/nobackup/ilz2/bayrak/documents/cool_hc.txt'
 
 # mask file
 image_mask = os.path.join('/nobackup/ilz2/bayrak/subjects_group',
                           'mni3_rest_gm_mask.nii.gz')
+
+## check if any of subjects has voxels, which has no signal
+# mask_check_all_subjects(fname, data_dir, image_mask)
+
+# subject_id
+subject_id = sys.argv[1]
 
 image_rest = os.path.join(data_dir, subject_id, 
                           'preprocessed/func', 
