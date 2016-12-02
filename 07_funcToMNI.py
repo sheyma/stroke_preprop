82de92bef8725d2248fb39fa0d5336619969bb8c
Şeyma Bayrak <sheymaba@gmail.com>
2016-12-02 01:14:13 +0100

    if subject is at day 01 ...

--- 07_funcToMNI.py
+++ 07_funcToMNI.py
@@ -3,7 +3,7 @@ import nipype.interfaces.ants as ants
 
 # data dir's 
 data_dir = '/nobackup/ilz2/bayrak/subjects'
-mni_dir  = '/nobackup/ilz2/bayrak'
+mni_dir  = '/nobackup/ilz2/bayrak/subjects_group'
 # subject id
 subject_id = sys.argv[1]
 
@@ -19,7 +19,7 @@ os.chdir(work_dir)
 img_rest = os.path.join(data_dir, subject_id,
 			'preprocessed/func/', 'rest_preprocessed.nii.gz')
 
-if subject_id[5:8] == 'd00':
+if subject_id[5:8] == 'd01':
 
 	# ants transform matrices (T1d00 -->> mni3mm)
 	ants_dir = os.path.join(data_dir, subject_id,  
@@ -39,41 +39,39 @@ if subject_id[5:8] == 'd00':
 		 		      os.path.join(bbre_dir, 						   						   'rest2anat_itk.mat')]
 	at.inputs.reference_image  = os.path.join(mni_dir, 
 						  'MNI152_T1_3mm_brain.nii.gz')
-	at.inputs.interpolation    = 'BSpline'
+	at.inputs.interpolation    = 'NearestNeighbor'
 	at.inputs.invert_transform_flags = [False, False, False]
 	at.inputs.output_image     = os.path.abspath('rest_preprocessed2mni.nii.gz')
 	#print at.cmdline
 	at.run()
 
-else:
-	subject_day00 = subject_id[0:5] + 'd00'
-	
-	# bbregister transform matrices (rest_dXX -->> T1_dXX)	
-	bbre_dir = os.path.join(data_dir, subject_id,
-			        'preprocessed/func/coregister/transforms2anat')
-	# fsl-flirt transform matrices (T1_XX -->> T1_d00)
-	flirt_dir = os.path.join(data_dir, subject_id,
-				 'preprocessed/anat/transforms2day00')
-	# ants transform matrices (T1_d00 -->> mni3mm)
-	ants_dir = os.path.join(data_dir, subject_day00,  
-				'preprocessed/anat/transforms2mni')
-	
+elif subject_id[5:8] != 'd01':
+	print "YESS"
+	subject_dayX = subject_id[0:5] + 'd01'
+		
 	at			   = ants.ApplyTransforms()
 	at.inputs.input_image_type = 3
 	at.inputs.input_image      = img_rest
-	at.inputs.transforms	   = [os.path.join(ants_dir, 						   						   'transform1Warp.nii.gz'),
-		      		      os.path.join(ants_dir, 						   						   'transform0GenericAffine.mat'),
-				      os.path.join(flirt_dir,
-					    'transform_day00_itk.mat'),
-		 		      os.path.join(bbre_dir, 						   						    'rest2anat_itk.mat')]
 	at.inputs.reference_image  = os.path.join(mni_dir, 
 						  'MNI152_T1_3mm_brain.nii.gz')
-	at.inputs.interpolation    = 'BSpline'
+	
+	at.inputs.transforms	   = [os.path.join(data_dir, subject_dayX,
+				      'preprocessed/anat/transforms2mni/' 						   					      'transform1Warp.nii.gz'),
+		      		      os.path.join(data_dir, subject_dayX,
+				       'preprocessed/anat/transforms2mni/'	 	
+				       'transform0GenericAffine.mat'),
+		 		      os.path.join(data_dir, subject_dayX,
+				       'preprocessed/func/coregister/transforms2anat',
+				       'rest2anat_itk.mat'),
+				      os.path.join(data_dir, subject_id,
+				       'preprocessed/func/transforms2rest01',
+				       'transform_day01_itk.mat')]
+
+	at.inputs.interpolation    = 'NearestNeighbor'
 	at.inputs.invert_transform_flags = [False, False, False, False]
 	at.inputs.output_image     = os.path.abspath('rest_preprocessed2mni.nii.gz')
 	print at.cmdline
 	at.run()
-
 
 
 
