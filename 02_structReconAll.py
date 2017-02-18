f6f0faa19669dd0b6a0aa1d870e20e6c0aed1af2
Şeyma Bayrak <sheymaba@gmail.com>
2017-02-18 14:51:00 +0100

    add more arguments and documentary

--- 02_structReconAll.py
+++ 02_structReconAll.py
@@ -1,27 +1,26 @@
 """
-How to run:
-$ python 02_structReconAll.py hc01_d00
+subject_id = sd51_d01 
+data_dir   = /data/pt_mar006/subjects
+free_dir   = /data/pt_mar006/freesurfer
+
+Usage:
+$ python 02_structReconAll.py <subject_id> <data_dir> <free_dir>
 """
+
 from nipype.interfaces.freesurfer.preprocess import ReconAll
 import os, glob, sys
 
-data_dir = '/nobackup/ilz2/bayrak/subjects'
-
 # subject_id and T1 scan directory: user given
 subject_id = sys.argv[1]
+data_dir   = sys.argv[2]
+free_dir   = sys.argv[3]
 
 Tscan 	   = os.path.join(data_dir, subject_id, 
 	    		 'nifti/mprage', 'T1.nii.gz')
-print Tscan
-# define working dir
-work_dir = os.path.join(data_dir, subject_id, 'freesurfer')
-
-# go into working dir
-os.chdir(work_dir)
 
 # run ReconAll
 recon_all = ReconAll()
 recon_all.inputs.subject_id   = subject_id
-recon_all.inputs.subjects_dir = '/nobackup/ilz2/bayrak/freesurfer'
+recon_all.inputs.subjects_dir = free_dir
 recon_all.inputs.T1_files     = Tscan
 recon_all.run()
