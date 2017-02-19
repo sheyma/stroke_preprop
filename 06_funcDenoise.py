6f47a20393f92640ca88c3b09c445dfed71a4871
Şeyma Bayrak <sheymaba@gmail.com>
2017-02-19 20:43:17 +0100

    add optional argument for intra-subject registration, eg. inverse of sd51_d00-> sd51_d01 -> T1d01

--- 06_funcDenoise.py
+++ 06_funcDenoise.py
@@ -1,3 +1,16 @@
+"""
+    # artefact detection (calculating outliers)
+    # calculates motion regressors given motion corrections parameters 
+    # creates binary masks for wm + csf, projecting to functional space
+    # denoising with CompCor, normalization and band-pass filtering
+    ### optional argument for intra-subject registration 
+    
+data_dir       = '/data/pt_mar006/subjects'
+subject_id     = 'sd51_d00'
+dayXX          = 'd01'
+Usage:
+    $ python 06_funcDenoise.py <data_dir> <subject_id> [dayXX]
+"""
 import os, sys
 import matplotlib
 matplotlib.use('Agg')
@@ -9,11 +22,8 @@ import nipype.interfaces.ants as ants
 from functions import motion_regressors
 from functions import nilearn_denoise
 
-# data dir's 
-data_dir  = '/nobackup/ilz2/bayrak/subjects'
-
-# subject id
-subject_id = sys.argv[1]
+data_dir   = sys.argv[1]
+subject_id = sys.argv[2]
 
 # define working dir
 work_dir = os.path.join(data_dir, subject_id, 
@@ -32,7 +42,7 @@ params_func = os.path.join(func_dir, 'rest_roi.nii.gz.par')
 # binary mask produced by skull stripping
 mask_func = os.path.join(func_dir, 'corr_rest_roi_brain_mask.nii.gz')
 
-# STEP #1 artefact detection ###########################
+# STEP #1 artefact detection 
 work_dir_artefact = os.path.join(os.getcwd(), 'artefact')
 if not os.path.exists(work_dir_artefact):
 	os.makedirs(work_dir_artefact)
@@ -51,7 +61,7 @@ ad.run()
 # get the name of the outlier file produced by ad 
 outlier_file = os.path.abspath('art.corr_rest_roi_outliers.txt')
 
-# STEP #2 motion regressors###################################
+# STEP #2 motion regressors 
 os.chdir(work_dir)
 work_dir_regress = os.path.join(os.getcwd(), 'regress')
 if not os.path.exists(work_dir_regress):
@@ -61,78 +71,112 @@ os.chdir(work_dir_regress)
 motionreg_file = motion_regressors(params_func,
 				   order=2, derivatives=1)[0]
 
-## STEP #3 #####################################################
+## STEP #3 projecting brain mask and wm+csf mask to rest space
 os.chdir(work_dir)
 work_dir_mask= os.path.join(os.getcwd(), 'mask')
 if not os.path.exists(work_dir_mask):
 	os.makedirs(work_dir_mask)
 os.chdir(work_dir_mask)
 
-# get white matter & CSF mask
+#create names...
+wmcsf_mask_func = os.path.abspath('wmcsf_mask_func.nii.gz')
+brain_mask_func = os.path.abspath('brain_mask_func.nii.gz')
 
-# get aparc+aseg.mgz from recon_all dir
-aparc_aseg = os.path.join(data_dir, subject_id,
-			  'freesurfer/mri', 'aparc+aseg.mgz')
+if len(sys.argv) > 3:
+    # e.g. dayXX = 'd01'
+    dayXX = sys.argv[3] 
+    # get wmcsf_mask from dayXX
+    wmcsf_mask = os.path.join(data_dir, subject_id[:5] + dayXX,
+                              'preprocessed/func/denoise/mask/'
+                              'wmcsf_mask.nii.gz')
+    
+    # get brain mask from dayXX
+    brain_mask = os.path.join(data_dir, subject_id[:5] + dayXX,
+                              'preprocessed/anat/brain_mask.nii.gz')  
+    # tranform matrices...
+    restToRest = os.path.join(data_dir, subject_id,
+                              'preprocessed/func/transforms2rest' + dayXX[1:],
+                              'transform_day' + dayXX[1:] + '_itk.mat')
 
-# define new filenames
-aparc_aseg_nifti  = os.path.abspath('aparc_aseg.nii.gz')
-wmcsf_mask        = os.path.abspath('wmcsf_mask.nii.gz')
-wmcsf_mask_func	  = os.path.abspath('wmcsf_mask_func.nii.gz')
-brain_mask_func   = os.path.abspath('brain_mask_func.nii.gz') 
+    restToAnat = os.path.join(data_dir, subject_id[:5] + dayXX,
+                              'preprocessed/func/coregister/transforms2anat',
+                              'rest2anat_itk.mat')
+    # reference mean rest...
+    restRefere = os.path.join(data_dir, subject_id,
+                              'preprocessed/func/realign',
+                              'mean_corr_rest_roi.nii.gz')
 
-# convert *mgz into *nii.gz
-mricon = fs.MRIConvert()
-mricon.inputs.in_file  = aparc_aseg
-mricon.inputs.out_file = aparc_aseg_nifti
-mricon.inputs.out_type = 'niigz'
-mricon.run()
+    # project wmcsf mask from dayXX to functional space of subject_id     
+    at			       = ants.ApplyTransforms()
+    at.inputs.input_image      = wmcsf_mask
+    at.inputs.output_image     = wmcsf_mask_func
+    at.inputs.reference_image  = restRefere
+    at.inputs.transforms       = [restToRest, restToAnat]
+    at.inputs.invert_transform_flags = [True, True]
+    at.inputs.interpolation    = 'NearestNeighbor'
+    at.run()
+    
+    # project brain_mask from dayXX to functional space of subject_id
+    at			   = ants.ApplyTransforms()
+    at.inputs.input_image      = brain_mask
+    at.inputs.output_image     = brain_mask_func
+    at.inputs.reference_image  = restRefere
+    at.inputs.transforms       = [restToRest, restToAnat]
+    at.inputs.interpolation    = 'NearestNeighbor'
+    at.inputs.invert_transform_flags = [True, True]
+    at.run()
+    
+else:
+    # create names...
+    wmcsf_mask      = os.path.abspath('wmcsf_mask.nii.gz') 
 
-# create wmcsf mask
-get_mask = fs.Binarize()
-get_mask.inputs.in_file     = aparc_aseg_nifti
-get_mask.inputs.wm_ven_csf  = True
-get_mask.inputs.erode       = 2
-get_mask.inputs.out_type    = 'nii.gz'
-get_mask.inputs.binary_file = wmcsf_mask
-get_mask.run()
+    # get aparc+aseg from /anat/ dir
+    aparc_aseg = os.path.join(data_dir, subject_id, 
+                              'preprocessed/anat/aparc+aseg.nii.gz')
+    # create wmcsf mask
+    get_mask = fs.Binarize()
+    get_mask.inputs.in_file     = aparc_aseg
+    get_mask.inputs.wm_ven_csf  = True
+    get_mask.inputs.erode       = 2
+    get_mask.inputs.out_type    = 'nii.gz'
+    get_mask.inputs.binary_file = wmcsf_mask
+    get_mask.run()
 
-### project wmcsf mask to functional space ###
-transform_matrix = os.path.join(data_dir, subject_id, 
+    # project wmcsf mask to functional space (with bbregister outputs) 
+    transform_matrix = os.path.join(data_dir, subject_id, 
 			'preprocessed/func/coregister/transforms2anat',
 			'rest2anat_itk.mat')
+    rest_mean        = os.path.join(data_dir, subject_id,
+                                    'preprocessed/func/realign',
+                                    'mean_corr_rest_roi.nii.gz')
 
-# mean rest scan after realignment
-rest_mean  = os.path.join(data_dir, subject_id,
-			  'preprocessed/func/realign',
-			  'mean_corr_rest_roi.nii.gz')
+    at			   = ants.ApplyTransforms()
+    at.inputs.input_image      = wmcsf_mask
+    at.inputs.transforms       = [transform_matrix]
+    at.inputs.reference_image  = rest_mean
+    at.inputs.interpolation    = 'NearestNeighbor'
+    at.inputs.invert_transform_flags = [True]
+    at.inputs.output_image     = wmcsf_mask_func
+    at.run()
 
-at			   = ants.ApplyTransforms()
-at.inputs.input_image      = wmcsf_mask
-at.inputs.transforms       = [transform_matrix]
-at.inputs.reference_image  = rest_mean
-at.inputs.interpolation    = 'NearestNeighbor'
-at.inputs.invert_transform_flags = [True]
-at.inputs.output_image     = wmcsf_mask_func
-at.run()
+    # projecting brain_mask to functional space
+    brain_mask = os.path.join(data_dir, subject_id,
+                            'preprocessed/anat/brain_mask.nii.gz')
 
-### projecting brain_mask to functional space
-brain_mask = os.path.join(data_dir, subject_id,
-			'preprocessed/anat/', 'brain_mask.nii.gz')
-
-at			   = ants.ApplyTransforms()
-at.inputs.input_image      = brain_mask
-at.inputs.transforms       = [transform_matrix]
-at.inputs.reference_image  = rest_mean
-at.inputs.interpolation    = 'NearestNeighbor'
-at.inputs.invert_transform_flags = [True]
-at.inputs.output_image     = brain_mask_func
-at.run()
+    at			   = ants.ApplyTransforms()
+    at.inputs.input_image      = brain_mask
+    at.inputs.transforms       = [transform_matrix]
+    at.inputs.reference_image  = rest_mean
+    at.inputs.interpolation    = 'NearestNeighbor'
+    at.inputs.invert_transform_flags = [True]
+    at.inputs.output_image     = brain_mask_func
+    at.run()
 
 # STEP #4 denoising with CompCor, normalization and band-pass filtering...
-
 os.chdir(work_dir)
 os.chdir(work_dir_regress)
 
+# create name...
 rest_denoised = os.path.join(data_dir, subject_id, 'preprocessed/func/',
 			     'rest_preprocessed.nii.gz')
 
@@ -140,5 +184,3 @@ nilearn_denoise(in_file = img_func, brain_mask = mask_func,
 		wm_csf_mask = wmcsf_mask_func, motreg_file = motionreg_file,
 		outlier_file = outlier_file, bandpass = [0.1, 0.01],
 		tr = 2.3, img_fname = rest_denoised)
-
-
