c799eec0cda483264d61e008f2170f07dbc37465
Şeyma Bayrak <sheymaba@gmail.com>
2017-03-04 13:48:12 +0100

    very cool colormapped histogram

--- plot_histo.py
+++ plot_histo.py
@@ -1,34 +1,24 @@
 import os, sys
-from nilearn import masking
+import nibabel as nb 
+from nilearn import masking, plotting
 import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
-import nibabel as nb
-from nipype.interfaces.fsl import MultiImageMaths
-import h5py
-import pickle
 
-#comp_file  = '/data/pt_mar006/subjects_group/mni3_component_1.nii.gz'
-#subject_id = 'sd27'
+subject_id = 'sd27'
 
-comp_file  = sys.argv[1]
-subject_id = sys.argv[2]
+component = '/data/pt_mar006/subjects_group/mni3_component_1.nii.gz'
 
 data_dir   = '/data/pt_mar006/stroke_intrasubject/'
 
-new_name = os.path.splitext(os.path.splitext(os.path.basename(comp_file))[0])[0]
-os.chdir(os.path.join(data_dir, subject_id))
+gm_mask  = '/data/pt_mar006/subjects_group/mni3_rest_gm_mask.nii.gz'
 
-les_file = os.path.join(data_dir, subject_id, 'lesion/lesion_mask_mni.nii.gz')
+sbj_mask = os.path.join(data_dir, subject_id, 'subject_mask_final.nii.gz')
 
 lesOnGMmask = os.path.join(data_dir, subject_id, 'subject_mask_03.nii.gz')
 
 concordance = os.path.join(data_dir, subject_id, 'conc_ccc.nii.gz')
-
-sbj_mask = os.path.join(data_dir, subject_id, 'subject_mask_final.nii.gz')
-
-gm_mask  = '/data/pt_mar006/subjects_group/mni3_rest_gm_mask.nii.gz'
 
 # Step 1, find indexes of voxels, where SD has concordance value
 mask_array = nb.load(sbj_mask).get_data()
@@ -36,104 +26,114 @@ x    = np.where(mask_array==1)[0]
 y    = np.where(mask_array==1)[1]
 z    = np.where(mask_array==1)[2]
 
-# Step 2, find voxels on comp_file, which correspond to lesion on SD
-A = masking.apply_mask(comp_file, lesOnGMmask)
-A = A.reshape((len(A), 1))
+COMP = np.array(nb.load(component).get_data())
+comp_min  = COMP[x,y,z].min()
+comp_max  = COMP[x,y,z].max()
 
-print "number of lesioned voxels ", A.shape
-
-## Step 3, get the component boundaries and its distribution on SD
-comp_hc  = masking.apply_mask(comp_file, sbj_mask)
-comp_min = comp_hc.min()
-comp_max = comp_hc.max() 
-
-COMP          = np.array(nb.load(comp_file).get_data())
 CONCORDANCE   = np.array(nb.load(concordance).get_data())
 
-##for tot_box_num in range(50,1000,50):
-for tot_box_num in range(20, 21):
-    print "total box number: ", tot_box_num
-    
-    # bins' length should be 1 term longer than total box number
-    bins = np.linspace(comp_min, comp_max, tot_box_num + 1)
-      
-    # for each comp element, find which bin it falls into 
-    inds = np.digitize(COMP[x, y, z], bins)
+#find voxels on comp_file, which correspond to lesion on SD
+lesion = masking.apply_mask(component, lesOnGMmask)
+lesion = lesion.reshape((len(lesion), 1))
 
-    # how many elements does each bin have
-    cnt_comp = np.histogram(COMP[x, y, z], bins)[0]
-    
-    tmp_x = {}
-    tmp_y = {}
-    tmp_z = {}
+# Step 2, binning...
+tot_box_num = 100
+bins = np.linspace(comp_min, comp_max, tot_box_num+1)
 
-    box_concordance = {}
-    box_proportion  = {}
+# for each comp element, find which bin it falls into 
+inds = np.digitize(COMP[x, y, z], bins)
 
-    # how many elements in each bin correspond to the lesion
-    cnt_les  = np.histogram(A, bins)[0]
-   
-    # go into each bin iteratively and find x,y,z coords of points there
-    for box_num in range(inds.min(), inds.max(), 1):
-        
-        # which elements along component fall into the current box
-        box_inds = np.where(inds == box_num)
-    
-        # reverse-indexing: find x,y,z of elements in current box
-        tmp_x[box_num] = x[box_inds]
-        tmp_y[box_num] = y[box_inds]
-        tmp_z[box_num] = z[box_inds]
+# how many elements does each bin have
+cnt_comp = np.histogram(COMP[x, y, z], bins)[0]
+cnt_les  = np.histogram(lesion, bins)[0]
 
-        #print COMP[tmp_x[box_num], tmp_y[box_num], tmp_z[box_num]].mean()
-     
-        voxel_values      = CONCORDANCE[tmp_x[box_num], tmp_y[box_num], tmp_z[box_num]]
-        box_concordance[box_num] = voxel_values.mean()
+tmp_x = {}
+tmp_y = {}
+tmp_z = {}
 
-        prop = cnt_les[box_num -1 ] / float(cnt_comp[box_num - 1]) * 100
-        box_proportion[box_num] = prop
+box_concordance = {}
 
-    begin = bins[0] + (bins[1]-bins[0]) / 2.0
-    end   = bins[-1] - (bins[1]-bins[0]) / 2.0
-    new_bins = np.linspace(begin, end, len(bins)-1 )
+box_proportion  = {}
 
+# go into each bin iteratively and find x,y,z coords of points there
+for box_num in range(inds.min(), inds.max(), 1):
 
-##### plotting
+    # which elements along component fall into the current box
+    box_inds = np.where(inds == box_num)
 
-fig, ax1 = plt.subplots(figsize=(17, 11))
+    # reverse-indexing: find x,y,z of elements in current box
+    tmp_x[box_num] = x[box_inds]
+    tmp_y[box_num] = y[box_inds]
+    tmp_z[box_num] = z[box_inds]
 
-ax1.hist(COMP[x, y, z], bins, normed=True, color='orange')
+    #print COMP[tmp_x[box_num], tmp_y[box_num], tmp_z[box_num]].mean()
 
-ax1.plot(new_bins, box_proportion.values(), 'r', linewidth=5, 
-             color='red', label='lesion %')
+    voxel_values      = CONCORDANCE[tmp_x[box_num], tmp_y[box_num], tmp_z[box_num]]
+    box_concordance[box_num] = voxel_values.mean()
 
-# make the y-axis label and tick labels match the line color
-concordance_type = 'CCC'
+    prop = cnt_les[box_num -1 ] / float(cnt_comp[box_num - 1]) * 100
+    box_proportion[box_num] = prop
 
+# plotting...
+fig  = plt.figure(figsize=(17,15))
 
-ax2 = ax1.twinx()
-ax2.plot(new_bins, box_concordance.values(), 'go', markersize=15, 
-         label=concordance_type)
-ax2.set_ylabel(concordance_type, color='g', fontsize=40)
+bins = np.linspace(comp_min, comp_max, tot_box_num)
 
-for tl in ax2.get_yticklabels():
+# MANUAL COLORBAR
+#cmap = cmocean.cm.ice
+#cmap = plt.cm.get_cmap('gnuplot')
+#norm = mpl.colors.Normalize(vmin=comp_min, vmax=comp_max)
+#ax1 = fig.add_axes([1.2, 0.05, 0.05, 0.5])
+#mycb = mpl.colorbar.ColorbarBase(ax1, cmap=cmap,norm=norm,
+#                                 orientation='vertical', 
+#                                 ticks=list(np.linspace(a,b,5)),
+#                                 format='%1.2f')
+
+# plot connectivity component along the brain
+ax1 = fig.add_subplot(2,1,1,)
+
+display = plotting.plot_stat_map(component, colorbar=True, cmap='gnuplot',                    
+                                 figure=fig, annotate=True, black_bg=True, 
+                                 cut_coords=[-10, 0, 22], axes=ax1,
+                                 symmetric_cbar=False)
+
+display.annotate(size=30)
+
+cm = display._cbar.get_cmap()
+cbar_ticks = display._cbar._ticker()[1]
+
+import matplotlib
+matplotlib.rcParams.update({'font.size': 25})
+
+# plot histogram of the component
+ax = fig.add_subplot(2,1,2,)
+
+n, bins, patches = ax.hist(COMP[x, y, z], bins=bins, normed=1)
+ax.xaxis.set_ticks(np.array(cbar_ticks, dtype='float64'))
+
+ax.plot(0.5* (bins[1] - bins[0]) + bins, box_proportion.values(), 'r',
+         linewidth=5, label='lesion %')
+ax.legend(frameon=False)
+
+bin_centers = 0.5 * (bins[:-1] + bins[1:])
+col = bin_centers - min(bin_centers)
+col /= max(col)
+for c, p in zip(col, patches):
+    plt.setp(p, 'facecolor', cm(c))
+
+#ax.set_title(subject_id)
+#ax.set_ylabel('number of voxels')
+#ax.set_xlabel('component spectrum')
+
+ax3 = ax.twinx()
+
+ax3.plot(0.5* (bins[1] - bins[0]) + bins, box_concordance.values(), 'go', 
+         markersize=7)
+#       label=concordance_type)
+#ax3.set_ylabel(concordance_type, color='g')
+
+for tl in ax3.get_yticklabels():
     tl.set_color('g')
-
-ax1.set_title(subject_id, fontsize=40)
-ax1.set_ylabel('number of voxels', fontsize=40)
-ax1.set_xlabel('component spectrum', fontsize=40)
-for tl in ax1.get_yticklabels():
-    tl.set_color('k')
-
-plt.legend(frameon=False)
-#plt.savefig("/data/pt_mar006/figures/fig_%s_%s.png" % 
-#            (new_name, subject_id))
-
-plt.savefig("/data/pt_mar006/tmp/simdi.png")
+ 
 #plt.show()
-
-#plt.scatter(c2, c1, c=A, cmap='jet')
-#plt.colorbar()
-#plt.savefig('/data/pt_mar006/mmm.eps')
-
-print "yess"
-
+#fig.savefig('/data/pt_mar006/figures_march/tmp_histo.png')
