bceeaaba0bc53d44a367fc90e5c5b54caf55c569
Şeyma Bayrak <sheymaba@gmail.com>
2017-05-28 14:34:50 +0200

    dilate lesion, remove it from gray matter, keep the original lesion on gm

--- 14_strokeMask.py
+++ 14_strokeMask.py
@@ -1,46 +1,57 @@
 """
-    get intra-subject rest mask for a stroke patient's different day measures 
-    (subject_mask_01.nii.gz), multiply this rest mask with the group-level-
-    healty-control-rest-and-gm-mask (subject_mask_02.nii.gz), find intersection
-    of stroke lesion on top of previous mask (subject_mask_03.nii.gz), finally
-    subtract lesion intersection from the mask (subject_mask_final.nii.gz)
+    dilate the lesion mask on mni space,
+    get intra-subject rest mask for a stroke patient's different day measures, 
+    multiply this rest mask with the group-level-healty-gm-mask, 
+    find the intersection of stroke lesion along the gm-mask,
+    subtract lesion intersection from the mask 
 
-gm_mask    = '/data/pt_mar006/subjects_group/mni3_rest_gm_mask.nii.gz'
 data_dir   = '/data/pt_mar006/subjects/'
-subject_id = 'sd51'
+out_dir    = '/data/pt_mar006/subjects_masks' 
+gm_mask    = '/data/pt_mar006/subjects_group/mni3_rest_gm_mask.nii.gz'
+subject_id = 'sd27'
 X          = 'd00'  #### lesion day
 
 Usage:
-    $ python 14_strokeMask.py  <gm_mask> <data_dir> <subject_id> <X>
+	$ python 14_strokeMask_dilated.py <d_dir> <o_dir> <gm_mask> <sbj_id> <X>	
 """
+
 import os, sys, glob
+from nipype.interfaces.fsl.maths import DilateImage
+from nipype.interfaces.fsl import MultiImageMaths
 from nilearn import masking
 import numpy as np
-from hcp_corr import corrcoef_upper
-import h5py
-import numexpr as ne
-from nipype.interfaces.fsl import MultiImageMaths
-from nipype.interfaces.fsl.maths import MathsCommand
-from nipype.interfaces import afni
-from hcp_corr import corrcoef_upper, N_original, upper_to_down
 
+data_dir   = sys.argv[1]
+out_dir    = sys.argv[2]
+gm_mask    = sys.argv[3]
+subject_id = sys.argv[4]
+X          = sys.argv[5]  
 
-gm_mask    = sys.argv[1]
-data_dir   = sys.argv[2]
-subject_id = sys.argv[3]
-X          = sys.argv[4]  
-
-les_mask   = os.path.join(data_dir, subject_id + '_' + X, 
-                         'lesion/lesion_mask_mni.nii.gz')
- 
-#### Step 1 ############################################################
+lesion_mask   = os.path.join(data_dir, subject_id + '_' + X, 
+                            'lesion/lesion_mask_mni.nii.gz')
 
 # define working dir
-work_dir = os.path.join('/data/pt_mar006/stroke_intrasubject', subject_id) 
+work_dir = os.path.join(out_dir, subject_id) 
 if not os.path.exists(work_dir):
 	os.makedirs(work_dir)
 # go into working dir
 os.chdir(work_dir)
+
+
+lesion_mask_dil = os.path.join(work_dir, 
+			       'lesion_mask_mni_dilated.nii.gz')
+print lesion_mask_dil
+
+###### Step 1: dilating lesion mask #############################
+dilate = DilateImage()
+dilate.inputs.in_file      = lesion_mask
+dilate.inputs.operation    = 'modal'
+dilate.inputs.out_file     = lesion_mask_dil
+#print dilate.cmdline
+dilate.run()
+
+
+##### Step 2: get intra-subject gm mask #########################
 
 rest_list = []
 rest_str  = []
@@ -58,43 +69,48 @@ for name in glob.glob(data_dir + subject_id + '*' +
 
 op_string_rest = " ".join((rest_str))
 
-# get intra-subject rest mask
 maths = MultiImageMaths()
 maths.inputs.in_file       = rest_list[0]
 maths.inputs.op_string     = op_string_rest
 maths.inputs.operand_files = rest_list[1:]
-maths.inputs.out_file      = 'subject_mask_01.nii.gz'
+maths.inputs.out_file      = 'rest_intra_mask.nii.gz'
 maths.run()
 
-###### Step 2 #########################################
+##### Step 3: gm mask lying on health controls ##################
 
 maths = MultiImageMaths()
-maths.inputs.in_file       = 'subject_mask_01.nii.gz'
+maths.inputs.in_file       = 'rest_intra_mask.nii.gz'
 maths.inputs.op_string     = '-mul %s'
 maths.inputs.operand_files = gm_mask
-maths.inputs.out_file      = 'subject_mask_02.nii.gz'
+maths.inputs.out_file      = 'gm_mask.nii.gz'
 maths.run()
 
-###### Step 3 ##########################################
-print "get lesion mask..."
-print les_mask
+##### Step 4: lesions lying on gm ##############################
+print "get lesion masks on gm..."
 
 maths = MultiImageMaths()
-maths.inputs.in_file       = 'subject_mask_02.nii.gz'
+maths.inputs.in_file       = 'gm_mask.nii.gz'
 maths.inputs.op_string     = '-mul %s'
-maths.inputs.operand_files = les_mask
-maths.inputs.out_file      = 'subject_mask_03.nii.gz'
+maths.inputs.operand_files = lesion_mask_dil
+maths.inputs.out_file      = 'lesion_mask_mni_dilated_gm.nii.gz'
 maths.run()
 
 maths = MultiImageMaths()
-maths.inputs.in_file       = 'subject_mask_02.nii.gz'
-maths.inputs.op_string     = '-sub %s'
-maths.inputs.operand_files = 'subject_mask_03.nii.gz'
-maths.inputs.out_file      = 'subject_mask_final.nii.gz'
+maths.inputs.in_file       = 'gm_mask.nii.gz'
+maths.inputs.op_string     = '-mul %s'
+maths.inputs.operand_files = lesion_mask
+maths.inputs.out_file      = 'lesion_mask_mni_gm.nii.gz'
 maths.run()
 
-###### Step 4 ############################################
+##### Step 5: remove dilated lesion from gm #####################
+maths = MultiImageMaths()
+maths.inputs.in_file       = 'gm_mask.nii.gz'
+maths.inputs.op_string     = '-sub %s'
+maths.inputs.operand_files = 'lesion_mask_mni_dilated_gm.nii.gz'
+maths.inputs.out_file      = 'gm_mask_no_lesion.nii.gz'
+maths.run()
 
+##### Step 6: check whether gm voxels have signals ##############
 def mask_check(rest, mask):
 	"""
 	rest: 4D nifti-filename
@@ -108,7 +124,7 @@ def mask_check(rest, mask):
 			cnt_zeros += 1
 	return cnt_zeros, matrix
 
-mask = os.path.abspath('subject_mask_final.nii.gz')
+mask = os.path.abspath('gm_mask_no_lesion.nii.gz')
 
 for img_rest in glob.glob(data_dir + subject_id[0:5] + '*' +
                       '/preprocessed/func/' +
@@ -118,5 +134,3 @@ for img_rest in glob.glob(data_dir + subject_id[0:5] + '*' +
     if voxel_zeros != 0:
         print "FUCK"
         sys.exit(1)    
-
-
