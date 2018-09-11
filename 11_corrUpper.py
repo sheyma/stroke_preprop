d7a52006cab2c1d7a8b674620b338483a57ab24a
Şeyma Bayrak <sheymaba@gmail.com>
2018-09-11 10:32:38 +0200

    averaging corr matrices with NaN or zero entries

--- 11_corrUpper.py
+++ 11_corrUpper.py
@@ -1,113 +1,112 @@
 import os, sys
 from nilearn import masking
 import numpy as np
-from hcp_corr import corrcoef_upper
 import h5py
 import numexpr as ne
 
-def fisher_r2z(R):
-    return ne.evaluate('arctanh(R)')
+#### Step 1 #### 
+#subject_list = ['hc01_d00', 'hc02_d00']
+#nameOUT      = 'firsttwo'
+# cond  = 'yes' or 'no'
 
-def mask_check(rest, mask):
-	"""
-	rest: 4D nifti-filename
-	mask: 3D nifti-filename
-	"""
-	matrix = masking.apply_mask(rest, mask)
-	matrix = matrix.T
-	cnt_zeros = 0 
-	for i in range(0, matrix.shape[0]):
-		if np.count_nonzero(matrix[i, :]) == 0:
-			cnt_zeros += 1
-	return cnt_zeros, matrix
+cond = sys.argv[1]
 
-def mask_check_all_subjects(fname, data_dir, mask):
-	"""
-	fname: text file having subject_id's
-	"""
-	with open(fname) as f:
-		content = f.readlines()
-		sbj_list = [x.strip('\n') for x in content]
+if cond == 'yes':
+    fname    = sys.argv[2]
+    nameOUT  = sys.argv[3]
+    data_in    = '/data/pt_mar006/subjects'
+    data_out   = '/data/pt_mar006/newtmp'
+    with open(fname) as f:
+        content = f.readlines()
+        subject_list = [x.strip('\n') for x in content]
+    print subject_list
+    
+    i = 0
+    for subject_id in subject_list:
+    
+        image_rest = os.path.join(data_in, subject_id, 
+                                  'preprocessed/func', 
+                                  'rest_preprocessed2mni_sm.nii.gz') 
+        image_mask = os.path.join('/data/pt_mar006/newtmp',
+                                  'gm_prob_mni3_ave_rest.nii.gz')
+        t_series   = masking.apply_mask(image_rest, image_mask)
+        t_series   = t_series.T
+        fc         = np.corrcoef(t_series)
+        fcbinar    = np.ones(fc.shape)
 
-	for subject_id in sbj_list:
-		rest = os.path.join(data_dir, subject_id,
-	                            'preprocessed/func', 
-	                            'rest_preprocessed2mni_sm.nii.gz')
-		[cnt_zeros, matrix] = mask_check(rest, mask)
-		print subject_id, cnt_zeros
+        mynanidx          = np.where(np.isnan(fc) == True)
+        fc[mynanidx]      = 0
+        fcbinar[mynanidx] = 0
+        
+        if i == 0:
+            SUM      = fc
+            SUMBINAR = fcbinar
+        else:
+            SUM      = ne.evaluate('SUM + fc')
+            SUMBINAR = ne.evaluate('SUMBINAR + fcbinar')
+        i += 1
+        print "DONE... ", subject_id, np.shape(SUM), np.max(SUMBINAR), np.min(SUMBINAR)
+    
+    sumout  = os.path.join(data_out, nameOUT + '.h5')
+    h       = h5py.File(sumout, 'w')
+    h.create_dataset("data", data = SUM)
+    h.close()
+    
+    binout = os.path.join(data_out, nameOUT + 'binary.h5')
+    k      = h5py.File(binout, 'w')
+    k.create_dataset("data", data = SUMBINAR)
+    k.close()
 
-def average_upper_matrix(fname, data_dir, out_file):
-	"""
-	fname: text file having subject_id's
-	"""
-	with open(fname) as f:
-		content = f.readlines()
-		sbj_list = [x.strip('\n') for x in content]
-		
-	i = 0
-	N = len(sbj_list)
-	for subject_id in sbj_list:
-		A = os.path.join(data_dir, subject_id,
-                                'preprocessed/func/connectivity',
-                                '%s_corrUpper.h5' % subject_id)
-		A_init = h5py.File(A, 'r')
-		A_data = A_init.get('data')
-		np.array(A_data)
+#### Step 2 #### average...
+if cond == 'no':
+    chopped_list = ['/data/pt_mar006/newtmp/firstfive.h5',
+                '/data/pt_mar006/newtmp/secondfive.h5',
+                '/data/pt_mar006/newtmp/thirdfive.h5',
+                '/data/pt_mar006/newtmp/fourthfive.h5',
+                '/data/pt_mar006/newtmp/fifthfive.h5',
+                '/data/pt_mar006/newtmp/sixththree.h5']
+    
+    binary_list =  ['/data/pt_mar006/newtmp/firstfivebinary.h5',
+                '/data/pt_mar006/newtmp/secondfivebinary.h5',
+                '/data/pt_mar006/newtmp/thirdfivebinary.h5',
+                '/data/pt_mar006/newtmp/fourthfivebinary.h5',
+                '/data/pt_mar006/newtmp/fifthfivebinary.h5',
+                '/data/pt_mar006/newtmp/sixththreebinary.h5']
+    
+    i = 0
+    for chopped, binary in zip(chopped_list, binary_list):
+    
+        print chopped
+        
+        C_init = h5py.File(chopped, 'r')
+        C_data = C_init.get('data')
+        C_data = np.array(C_data)
+        
+        B_init = h5py.File(binary, 'r')
+        B_data = B_init.get('data')
+        B_data = np.array(B_data)
+        
+        if i == 0:
+            SUM    = C_data
+            SUMBIN = B_data 
+        else:
+            SUM    = ne.evaluate('SUM + C_data')
+            SUMBIN = ne.evaluate('SUMBIN + B_data' ) 
+        i += 1
+    
+    print np.shape(SUM), np.max(SUM), np.min(SUM)    
+    print np.shape(SUMBIN), np.max(SUMBIN), np.min(SUMBIN)    
+    
+    ave    = np.divide(SUM, SUMBIN)
+    
+    print np.max(ave), np.min(ave)
+    
+    h       = h5py.File('/data/pt_mar006/newtmp/avecorr.h5', 'w')
+    h.create_dataset("data", data = ave)
+    h.close()
+    
+    
+    
 
-                if i == 0:
-			SUM = A_data
-		else:
-			SUM = ne.evaluate('SUM + A_data')
-		i += 1
-		print subject_id, SUM.shape
-		
-        SUM = ne.evaluate('SUM / N')
-        h = h5py.File(out_file, 'w') 
-        h.create_dataset("data", data=SUM)
-        h.close()
 
-	return SUM
-
-# data dir's 
-data_dir  = '/data/pt_mar006/subjects'
-
-# text file having subject_id's
-fname = '/data/pt_mar006/documents/cool_hc.txt'
-
-# mask file
-image_mask = os.path.join('/data/pt_mar006/subjects_group',
-                          'mni3_rest_gm_mask.nii.gz')
-
-# check if any of subjects has voxels, which has no signal
-mask_check_all_subjects(fname, data_dir, image_mask)
-
-# get average upper triangular of individual (fisher_r2z) correlations
-out_file = '/data/pt_mar006/subjects_group/corrFisherR2Z_upper.h5'
-ave_corr_fish_upper = average_upper_matrix(fname, data_dir, out_file)
-
-# subject_id
-subject_id = sys.argv[1]
-
-image_rest = os.path.join(data_dir, subject_id, 
-                          'preprocessed/func', 
-                          'rest_preprocessed2mni_sm.nii.gz') 
-
-[voxel_zeros, t_series] = mask_check(image_rest, image_mask)
-
-if voxel_zeros != 0:
-    print subject_id, " has zeros voxel!!!"
-    sys.exit(1)
-
-# Step 1: get correlation coefficients and Fisher r2z transform
-corr_upper = corrcoef_upper(t_series)
-corr_upper = fisher_r2z(corr_upper)
-
-# define working dir 
-work_dir = os.path.join(data_dir, subject_id,  
-			'preprocessed/func/connectivity') 
-os.chdir(work_dir)
-
-h = h5py.File("%s_corrUpper.h5" %subject_id, 'w')
-h.create_dataset("data", data=corr_upper)
-h.close()
 
